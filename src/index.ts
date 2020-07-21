b1c7c2003debfc4ff8c8b9d65cd1269445d9dfac
Natália Acedo <n.alves.acedo@gmail.com>
2020-07-21 11:35:01 -0300

    Arquivos services criados: HashGenerator, IdGenerator e TokenGenerator

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { AddressInfo } from "net";
+import cors from "cors";
+import { userRouter } from "./routes/UserRouter";
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors())
+app.use("/users/", userRouter);
+
+const server = app.listen(3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address.port}`);
+  } else {
+    console.error(`Falha ao rodar o servidor.`);
+  }
+});
